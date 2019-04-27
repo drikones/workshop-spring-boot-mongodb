27e360454e98778e307500296ddceac0263b3e7f
Adriano Rocha <adriano_rocha15@hotmail.com>
2019-04-27 20:21:17 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.nelioalves.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() { 
+		return userRepository.findAll();
+	}
+
+}
