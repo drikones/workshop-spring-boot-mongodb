d7a226312ad3c92cf813e08b82b2f5b05cedd4ba
Adriano Rocha <adriano_rocha15@hotmail.com>
2019-05-05 19:39:17 -0300

    Projeção dos dados do Autor com DTO

--- src/main/java/com/nelioalves/workshopmongo/domain/Post.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/Post.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.nelioalves.workshopmongo.dto.AuthorDTO;
+
 @Document(collection="post")
 public class Post implements Serializable {
 	
@@ -17,13 +19,13 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 	
 
 
 	public Post() {}
 	
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -59,10 +61,10 @@ public class Post implements Serializable {
 	public void setBody(String body) {
 		this.body = body;
 	}
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
---
+++ src/main/java/com/nelioalves/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,47 @@
+package com.nelioalves.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.nelioalves.workshopmongo.domain.User;
+
+public class AuthorDTO implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private String name;
+	
+	
+	public AuthorDTO() {}
+	
+	public AuthorDTO(User user) {
+		this.id = user.getId();
+		this.name = user.getName();
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+
+}
